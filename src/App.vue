69e2d99cddea70562cceeeabf9f37621359bd065
gab <corbanigabriele@gmail.com>
2024-09-27 16:37:20 +0200

    scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script>
 import { store } from './store.js'
 import AppHeader from './components/AppHeader.vue'
-import AppContent from './components/AppContent.vue'
 import AppFooter from './components/AppFooter.vue'
 
 export default {
   components: {
     AppHeader,
-    AppContent,
     AppFooter,
   },
   data() {
@@ -20,15 +18,24 @@ export default {
 </script>
 
 <template>
-
   <div id="app">
     <AppHeader></AppHeader>
-    <AppContent></AppContent>
+    <h1>Hello App!</h1>
+    <p>
+      <strong>Current route path:</strong> {{ $route.fullPath }}
+    </p>
+    <nav>
+      <RouterLink to="/">Go to Home</RouterLink>
+      <RouterLink to="/about">Go to About</RouterLink>
+    </nav>
+    <main>
+      <RouterView />
+    </main>
     <AppFooter></AppFooter>
-
   </div>
 
 </template>
+
 
 <style lang="scss">
 @use './assets/style/general.scss';
--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
+import router from './router'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* add icons to the library */
-library.add(fas, far, fab)
 
 const app = createApp(App)
 
-app.component('font-awesome-icon', FontAwesomeIcon)
-
-app.mount('#app')
+app.use(router).mount('#app')
